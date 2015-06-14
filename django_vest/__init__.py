6b12e4e55b9b6a97ddea2aaa577eaee911254acf
zero13cool <zero13cool@yandex.ru>
2015-06-15 00:09:38 +0300

    Up to version 0.1.3

--- django_vest/__init__.py
+++ django_vest/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'Vladimir Savin <zero13cool@yandex.ru>'
-__version__ = '0.1.2a'
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name="django-vest",
-    version="0.1.2a",
+    version="0.1.3",
 
     author="Vladimir Savin",
     author_email="zero13cool@yandex.ru",
