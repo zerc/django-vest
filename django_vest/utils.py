11d775c7c9195f8adbfcf4ef673f4767734a1534
zero13cool <zero13cool@yandex.ru>
2017-12-04 22:33:17 +0000

    Started Django update #8

--- django_vest/utils.py
+++ django_vest/utils.py
@@ -5,7 +5,11 @@ from importlib import import_module
 
 from django.conf import settings
 
-is_theme_dir = lambda d: d.endswith('_theme')
+
+def is_theme_dir(d):
+    """ A helper to test either the directory is theme-related or not.
+    """
+    return d.endswith('_theme')
 
 
 def load_object(s):
@@ -23,7 +27,22 @@ def load_object(s):
 def get_available_themes():
     """ Iterator on available themes
     """
-    for d in settings.TEMPLATE_DIRS:
+    for d in get_dirs():
         for _d in os.listdir(d):
             if os.path.isdir(os.path.join(d, _d)) and is_theme_dir(_d):
                 yield _d
+
+
+def get_dirs():
+    """ Iterator over themes-related dirs.
+    """
+    for backend in settings.TEMPLATES:
+        is_vest_backend = (
+            'django_vest' in ''.join(backend['OPTIONS'].get('loaders', []))
+        )
+
+        if not is_vest_backend:
+            continue
+
+        for d in backend['DIRS']:
+            yield d
--- exampleproject/urls.py
+++ exampleproject/urls.py
@@ -1,13 +1,12 @@
 # coding: utf-8
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
 
 import views
 
-urlpatterns = patterns(
-    '',
+admin.autodiscover()
+
+urlpatterns = [
     url(r'^$', views.IndexPage.as_view(), name='index'),
     url(r'^dark/$', views.dark_theme_page, name='dark_index'),
     url(r'^dark2/$', views.dark_theme_page_not_found,
@@ -15,4 +14,4 @@ urlpatterns = patterns(
     url(r'^restict/$', views.restict_access, name='restict_access'),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
